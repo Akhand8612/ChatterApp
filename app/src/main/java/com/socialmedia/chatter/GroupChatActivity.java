6838c4f525970b57984969bbedd2b256d7c1b71a
Akhand8612 <ak.s.rajput8612@gmail.com>
2022-04-20 00:48:18 +0530

    Chatter App

---
+++ app/src/main/java/com/socialmedia/chatter/GroupChatActivity.java
@@ -0,0 +1,94 @@
+package com.socialmedia.chatter;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.socialmedia.chatter.Adapters.ChatAdapter;
+import com.socialmedia.chatter.Models.MessageModel;
+import com.socialmedia.chatter.databinding.ActivityGroupChatBinding;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class GroupChatActivity extends AppCompatActivity {
+
+    ActivityGroupChatBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityGroupChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+
+        binding.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GroupChatActivity.this , MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final ArrayList<MessageModel> messageModels = new ArrayList<>();
+
+        final String senderId = FirebaseAuth.getInstance().getUid();
+        binding.userName.setText("Friends Group");
+        final ChatAdapter adapter = new ChatAdapter(messageModels , this);
+        binding.chatRecyclarView.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.chatRecyclarView.setLayoutManager(layoutManager);
+
+        database.getReference().child("Group Chat")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        messageModels.clear();
+                        for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                            MessageModel model = dataSnapshot.getValue(MessageModel.class);
+                            messageModels.add(model);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+        binding.send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String message  = binding.etMessage.getText().toString();
+                final MessageModel model = new MessageModel(senderId, message);
+                model.setTimestamp(new Date().getTime());
+                binding.etMessage.setText("");
+
+                database.getReference().child("Group Chat")
+                        .push()
+                        .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+
+                    }
+                });
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/socialmedia/chatter/SigninActivity.java
@@ -0,0 +1,168 @@
+package com.socialmedia.chatter;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
+import com.socialmedia.chatter.Models.Users;
+import com.socialmedia.chatter.databinding.ActivitySigninBinding;
+
+public class SigninActivity extends AppCompatActivity {
+
+    ActivitySigninBinding binding;
+    ProgressDialog progressDialog;
+    FirebaseAuth auth;
+    GoogleSignInClient mGoogleSignInClient;
+    FirebaseDatabase database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySigninBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        progressDialog = new ProgressDialog(SigninActivity.this);
+        progressDialog.setTitle("Login");
+        progressDialog.setMessage("Login to your account");
+
+        GoogleSignInOptions gso =  new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))//you can also use R.string.default_web_client_id
+                .requestEmail()
+                .build();
+
+       mGoogleSignInClient = GoogleSignIn.getClient(this , gso);
+
+        binding.btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(binding.etEmail.getText().toString() .isEmpty()){
+                    binding.etEmail.setError("Enter your email");
+                    return;
+                }
+                if(binding.etPassword.getText().toString() .isEmpty()){
+                    binding.etPassword.setError("Enter your Password");
+return;
+                }
+                progressDialog.show();
+
+                auth.signInWithEmailAndPassword(binding.etEmail.getText().toString() ,
+                        binding.etPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+
+                        if(task.isSuccessful()){
+                            Intent intent = new Intent(SigninActivity.this , MainActivity.class);
+                            startActivity(intent);
+
+                        }
+                        else{
+                            Toast.makeText(SigninActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+        binding.tvClickSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SigninActivity.this , SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        binding.btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+        if(auth.getCurrentUser()!=null){
+
+            Intent intent = new Intent(SigninActivity.this , MainActivity.class);
+            startActivity(intent);
+        }
+    }
+int RC_SIGN_IN = 65;
+    private void signIn(){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        //noinspection deprecation
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d("TAG", "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                Log.w("TAG", "Google sign in failed", e);
+            }
+        }
+    }
+   private void firebaseAuthWithGoogle(String idToken) {
+        // Got an ID token from Google. Use it to authenticate
+        // with Firebase.
+        AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
+        auth.signInWithCredential(firebaseCredential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = auth.getCurrentUser();
+
+                            Users users = new Users();
+                            users.setUserId(user.getUid());
+                            users.setUserName(user.getDisplayName());
+                            users.setProfilepic(user.getPhotoUrl().toString());
+                            database.getReference().child("Users").child(user.getUid()).setValue(users);
+
+                            Intent intent = new Intent(SigninActivity.this , MainActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(SigninActivity.this, "Sign in with Google", Toast.LENGTH_SHORT).show();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+                            //Snackbar.make(mBinding.mainLayout, "Authentication failed", Snackbar.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+    }
+}
